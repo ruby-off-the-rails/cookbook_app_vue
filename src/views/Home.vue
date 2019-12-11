ac20566fe6622f95d65fb5df20b2d39ebbceaf71
brian <riceb53@gmail.com>
2019-12-10 20:52:12 -0600

    complete new/create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <p>Add the title of your new recipe here<input type="text" v-model="newRecipeTitle"></p>
+    <p>Add the Chef of your new recipe here<input type="text" v-model="newRecipeChef"></p>
+    <p>Add the Ingredients of your new recipe here<input type="text" v-model="newRecipeIngredients"></p>
+    <p>Add the PrepTime of your new recipe here<input type="text" v-model="newRecipePrepTime"></p>
+    <p>Add the Directions of your new recipe here<input type="text" v-model="newRecipeDirections"></p>
     <button v-on:click="addRecipe()">Add a new recipe</button>
     <div v-for="recipe in recipes">
      <p>title: {{ recipe.title }}</p>
@@ -29,7 +34,12 @@ export default {
     return {
       message: "Welcome to actualize!",
       name: "Brian",
-      recipes: []
+      recipes: [],
+      newRecipeTitle: "",
+      newRecipeChef: "",
+      newRecipeIngredients: "",
+      newRecipePrepTime: "",
+      newRecipeDirections: "",
     };
   },
   created: function() {
@@ -44,15 +54,22 @@ export default {
       console.log('adding the receip...');
 
       var params = {
-        chef: "brian",
-        title: "croissant",
-        ingredients: "flour, butter, heat",
-        directions: "heat the flour",
-        prep_time: 10
+        chef: this.newRecipeChef,
+        title: this.newRecipeTitle,
+        ingredients: this.newRecipeIngredients,
+        directions: this.newRecipeDirections,
+        prep_time: this.newRecipePrepTime
       };
+
 
       axios.post("/api/recipes", params).then(response => {
         console.log(response.data);
+        this.recipes.push(response.data);
+        this.newRecipeTitle = "";
+        this.newRecipeChef = "";
+        this.newRecipeIngredients = "";
+        this.newRecipePrepTime = "";
+        this.newRecipeDirections = "";
       });
     }
   }
