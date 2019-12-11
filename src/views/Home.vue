46102e7a27d4e52caa1fc47dca8570d54b916abd
brian <riceb53@gmail.com>
2019-12-10 19:24:41 -0600

    index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
+    <div v-for="recipe in recipes">
+     <p>{{ recipe.title }}</p>
+     <p>{{ recipe.ingredients }}</p>
+     <br>
+
+     <hr>
+    </div>
+
     <h1>{{ message }}</h1>
+    <h1>{{ name }}</h1>
+    <h1>{{ recipes }}</h1>
   </div>
 </template>
 
@@ -8,14 +18,23 @@
 </style>
 
 <script>
+import axios from "axios";
+// var axios = require("axios");
+
 export default {
   data: function() {
     return {
-      message: "Welcome to actualize!"
+      message: "Welcome to actualize!",
+      name: "Brian",
+      recipes: []
     };
   },
   created: function() {
     console.log('i am in created');
+    axios.get("/api/recipes").then(response => {
+      console.log(response.data);
+      this.recipes = response.data;
+    });
   },
   methods: {}
 };
